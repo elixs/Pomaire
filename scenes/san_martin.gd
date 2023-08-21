142fb04a73fba2fade24e9b0accb3687c0730eb1
Elías Zelada <elixs.777@gmail.com>
2023-08-21 13:11:18 -0400

    player, menu and theme
    also helper debug class

--- scenes/san_martin.gd
+++ scenes/san_martin.gd
@@ -9,29 +9,48 @@ var gravity = 300
 @onready var animation_tree: AnimationTree = $AnimationTree
 @onready var playback = animation_tree.get("parameters/playback")
 
+@onready var pivot: Node2D = $Pivot
+
+
 func _ready() -> void:
 	animation_tree.active = true
+
 
 func _physics_process(delta: float) -> void:
 	var move_input = Input.get_axis("move_left", "move_right")
 	
+	
 	if not is_on_floor():
 		velocity.y += gravity * delta
 	
-	if Input.is_action_just_pressed("jump"):
+	
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = -jump_speed
+		Debug.dprint("jump")
+	
 	
 	velocity.x = move_toward(velocity.x, speed * move_input, acceleration * delta)
 	
 	move_and_slide()
 	
 	# animation
-	if abs(velocity.x) > 10 or move_input != 0:
-		playback.travel("run")
+	
+	if move_input != 0:
+		pivot.scale.x = sign(move_input)
+	
+	
+	if is_on_floor():
+		if abs(velocity.x) > 10 or move_input != 0:
+			playback.travel("run")
+		else:
+			playback.travel("idle")
 	else:
-		playback.travel("idle")
+		if velocity.y < 0:
+			playback.travel("jump")
+		else:
+			playback.travel("fall")
 	
 
-#func _input(event: InputEvent) -> void:
-#	if event.is_action_pressed("jump"):
-#		animation_player.play("jump")
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump"):
+		print("test")
