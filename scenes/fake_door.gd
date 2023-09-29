4d301c429c95606fdebe519daca9e032ef7941ed
Elías Zelada <elixs.777@gmail.com>
2023-09-29 12:27:31 -0300

    level manager

--- scenes/fake_door.gd
+++ scenes/fake_door.gd
@@ -2,6 +2,8 @@ extends Area2D
 
 @onready var message = $Message
 
+var launch_speed = 250
+
 var characters_inside: Array[CharacterBody2D] = []
 
 func _ready():
@@ -13,7 +15,7 @@ func _ready():
 func _input(event):
 	if event.is_action_pressed("interact"):
 		for body in characters_inside:
-			LevelManager.next_level()
+			launch_character(body)
 
 func _on_body_entered(body: Node):
 	if body is CharacterBody2D:
@@ -26,3 +28,7 @@ func _on_body_exited(body: Node):
 	characters_inside.erase(body)
 	if characters_inside.is_empty():
 		message.hide()
+
+func launch_character(character: CharacterBody2D):
+	var direction = Vector2(4 * randf_range(-1, 1), randf_range(-1, 0))
+	character.velocity = direction * launch_speed
--- scenes/ui/main_menu.gd
+++ scenes/ui/main_menu.gd
@@ -3,8 +3,6 @@ extends MarginContainer
 @onready var start: Button = %Start
 @onready var exit: Button = %Exit
 
-#var main = preload("res://scenes/main.tscn")
-@export var main: PackedScene
 
 func _ready() -> void:
 	start.pressed.connect(_on_start_pressed)
@@ -12,8 +10,7 @@ func _ready() -> void:
 
 
 func _on_start_pressed():
-	if main:
-		get_tree().change_scene_to_packed(main)
+	LevelManager.start_game()
 
 func _on_exit_pressed():
 	get_tree().quit()
